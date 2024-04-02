7d00566f3a05fd538baf4854b51471c84efd6e2e
Leandro Brognoli <85521339+Brogson@users.noreply.github.com>
2024-04-02 20:40:13 -0300

    finalização do curso de javascript

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,2,4,9,3]
+num[6] = 6
+num.push(7)
+console.log(`Nosso vetor é o ${num}`)
+num.sort()
+console.log(`Nosso vetor em forma crescente é o ${num}`)
+console.log(`Este vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
